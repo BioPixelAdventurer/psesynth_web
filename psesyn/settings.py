511d15aee4576e6dabc942fac5bd7ec04fb6bb43
BioPixelAdventurer <149067666+BioPixelAdventurer@users.noreply.github.com>
2023-11-16 14:44:59 +0100

    Update settings.py

--- psesyn/settings.py
+++ psesyn/settings.py
@@ -124,11 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-# NOT BAKIG
-# STATIC_URL = '/static/'
 
-# BAKING
-STATIC_URL = '/online/static/'
+STATIC_URL = '/static/'
+
+
 STATICFILES_DIRS = [BASE_DIR / 'psesyn_app' / 'static']
 
 
@@ -138,10 +137,8 @@ STATICFILES_DIRS = [BASE_DIR / 'psesyn_app' / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# NOT BAKING
-# MEDIA_URL = '/media/'
 
-# BAKING
-MEDIA_URL = '/online/media/'
+MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
