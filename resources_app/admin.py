4a5674139243077150add00061e73022c1f7c5b1
Paula <paulaegalindo@gmail.com>
2023-11-19 13:30:56 +0100

    UPDATES

--- resources_app/admin.py
+++ resources_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import PaperTemp, PubType
+from .models import PaperTemp, PubType, Author, Year
 
 admin.site.register(PaperTemp)
 admin.site.register(PubType)
+admin.site.register(Author)
+admin.site.register(Year)
 # Register your models here.
