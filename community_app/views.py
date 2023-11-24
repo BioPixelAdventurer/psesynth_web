5120f0abcb58cecb7804f23fe460d0ddbad54f90
Paula <paulaegalindo@gmail.com>
2023-11-24 16:03:11 +0100

    UPDATE:community new

--- community_app/views.py
+++ community_app/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
 from .models import Participant
 
-class ParticipantView(ListView):
-    model = Participant
-    context_object_name = 'instances'
-    template_name = 'community.html'
+class CoordView(View):
+    def get(self, request):
+        instances = Participant.objects.filter(roll_s__roll='Coordinator')
+        dictionary = {
+            'instances': instances,
+            'header_text':'Coordinators'
+        }
+        return render(request, 'coordinators.html', dictionary)
 
-    def get_queryset(self):
-        roll_in = self.request.GET.get('roll', '')
-        if roll_in:
-            queryset = Participant.objects.filter(roll_s__roll=roll_in)
-        else:
-            queryset = Participant.objects.all()
-
-        return queryset
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        roll_in = self.request.GET.get('roll', '')
-        context['header_text'] = f'{roll_in}s' if roll_in else 'Participants'
-        return context
+class ParticipantView(View):
+    def get(self, request):
+        instances = Participant.objects.filter(roll_s__roll='Participant')
+        dictionary = {
+            'instances': instances,
+            'header_text':'Participants'
+        }
+        return render(request, 'participants.html', dictionary)
